92e48967fdd684fda757e445861b697b38671e23
Jacob Franklin <jacob.franklin@healthtel.net>
2019-01-08 11:28:47 -0600

    refactor

--- config/orm.js
+++ config/orm.js
@@ -1,20 +1,5 @@
 var connection = require("./connection.js");
 
-//Converts object key/value pairs to SQL syntax
-function objToSql(ob) {
-    var arr = [];
-    for (var key in ob) {
-      var value = ob[key];
-      if (Object.hasOwnProperty.call(ob, key)) {
-        if (typeof value === "string" && value.indexOf(" ") >= 0) {
-          value = "'" + value + "'";
-        };
-        arr.push(key + "=" + value);
-      };
-    };
-    return arr.toString();
-};
-
 var orm = {
     selectAll: function(cols, table, cb){
         var queryString = "SELECT ?? FROM ??";
